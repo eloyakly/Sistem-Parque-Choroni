50253fcdde3ed26d13520c37c09f5b1ea9c5f754
Eloy PC <eloymalave71@gmail.com>
2026-03-19 23:06:01 -0400

    Inicializacion de proyecto parque choroni

---
+++ app/Http/Controllers/TipoApartamentoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoApartamento;
+use Illuminate\Http\Request;
+
+class TipoApartamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoApartamento $tipoApartamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipoApartamento $tipoApartamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TipoApartamento $tipoApartamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoApartamento $tipoApartamento)
+    {
+        //
+    }
+}
